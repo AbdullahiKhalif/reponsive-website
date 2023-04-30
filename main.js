d20bb06dc803a783612cf6a37e8157719383fbcd
Abdullahi-Khalif <92039904+ENG-KHALIIFA@users.noreply.github.com>
2023-04-30 19:20:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+let burger = document.querySelector(".fa-bars");
+let navigationBar = document.querySelector(".navigation-bar");
+burger.addEventListener("click", ()=>{
+    navigationBar.classList.toggle("active")
+})
